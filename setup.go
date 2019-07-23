df4c77cfea335481809776ff03b911452b7ce836
kapil Thacker <36205519+kapilth@users.noreply.github.com>
2019-07-22 18:26:20 -0700

    Update setup.go

--- setup.go
+++ setup.go
@@ -5,8 +5,8 @@ import (
 	"strings"
 
 	"github.com/coredns/coredns/core/dnsserver"
-	"github.com/coredns/coredns/plugin"
-	"github.com/coredns/coredns/plugin/proxy"
+	"github.com/coredns/plugin"
+	"github.com/coredns/proxy"
 
 	"github.com/caddyserver/caddy/caddy"
 	"github.com/miekg/dns"
